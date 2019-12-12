3d271d4ed3d56c491e0295041eb65e0539a603aa
Luís Ferreira <luis.guilherme.ferreira@hotmail.com>
2019-12-12 18:54:13 -0100

    Implemented AuthController and AuthService - plus, imported a new dependency

--- deltaclub/src/main/java/org/academiadecodigo/thunderstructs/controllers/AuthController.java
+++ deltaclub/src/main/java/org/academiadecodigo/thunderstructs/controllers/AuthController.java
@@ -4,6 +4,7 @@ package org.academiadecodigo.thunderstructs.controllers;
 import org.academiadecodigo.thunderstructs.models.User;
 import org.academiadecodigo.thunderstructs.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -12,22 +13,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+
 @Controller
+@RequestMapping("/login")
 public class AuthController {
 
 
     private AuthService authService;
 
 
-    @RequestMapping(method = RequestMethod.POST, value = {"/"})
-    public void login(){
+    @RequestMapping(method = RequestMethod.POST, value = {"/", ""})
+    public ResponseEntity<?> login(String userAndPass) {
 
+        String[] usernameAndPassword = userAndPass.split("#");
+
+        if (authService.authenticate(usernameAndPassword[0], usernameAndPassword[1])) {
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<User>submitRegistration(@Valid @RequestBody User user, BindingResult bindingResult, @PathVariable Integer id){
+    @RequestMapping(method = RequestMethod.POST, value = {""})
+    public ResponseEntity<?> submitRegistration(@Valid @RequestBody User user, BindingResult bindingResult){ //TODO: configure User properties
 
-        authService.addRegistration();
+        boolean status = authService.addRegistration(user);
+
+        if (bindingResult.hasErrors() || !status) {
+
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
