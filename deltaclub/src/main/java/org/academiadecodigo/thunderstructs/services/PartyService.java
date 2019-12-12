86a8ba7cf894cffcca46af8cd2cf869cd23f91aa
Vasco Sousa <az.thewolf.jr@gmail.com>
2019-12-12 15:49:41 -0100

    Implemented PartyController, PartyService Interface, and PartyService Imp

--- deltaclub/src/main/java/org/academiadecodigo/thunderstructs/services/PartyService.java
+++ deltaclub/src/main/java/org/academiadecodigo/thunderstructs/services/PartyService.java
@@ -1,6 +1,9 @@
 package org.academiadecodigo.thunderstructs.services;
 
+
 public interface PartyService {
 
+
+    void addUser(int id, int partyId);
 
 }
--- deltaclub/src/main/java/org/academiadecodigo/thunderstructs/services/PartyServiceImp.java
+++ deltaclub/src/main/java/org/academiadecodigo/thunderstructs/services/PartyServiceImp.java
@@ -1,20 +1,35 @@
 package org.academiadecodigo.thunderstructs.services;
 
 
+import org.academiadecodigo.thunderstructs.models.Party;
+import org.academiadecodigo.thunderstructs.models.User;
 import org.academiadecodigo.thunderstructs.utils.ClubDB;
+import org.academiadecodigo.thunderstructs.utils.errors.CustomerNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PartyServiceImp {
+public class PartyServiceImp implements PartyService{
 
     private ClubDB clubDB;
-
-
     private UserService userService;
 
 
-    public void addUser(int id, int partyID){
+    public void addUser(int id, int partyId){
+
+        try {
+            User user = clubDB.getUsers().get(id - 1);
+            Party party = clubDB.getParties().get(partyId - 1);
+
+            if (user == null || party == null) {
+                throw new CustomerNotFoundException();
+            }
+
+            party.getUsers().add(user);
+
+        }catch (CustomerNotFoundException ex){
+            System.out.println("No customer/party with that id");
+        }
 
     }
 
@@ -26,5 +41,22 @@ public class PartyServiceImp {
 
     public ClubDB getClubDB() {
         return clubDB;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    public static void main(String[] args) {
+
+        ClubDB clubDB = new ClubDB();
+        PartyServiceImp partyServiceImp = new PartyServiceImp();
+
     }
 }
