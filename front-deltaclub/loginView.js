ac90758f8f8d3023f35771226be9337321dfdb5d
Filipe António <filipe.aa1995@gmail.com>
2019-12-12 19:24:42 -0100

    continued on loginview

--- front-deltaclub/loginView.js
+++ front-deltaclub/loginView.js
@@ -1,9 +1,34 @@
 
 $(document).ready(function () {
+
+
 });
 
 
+function show(){
+    $(".registerform").show();
+}
 
-$("#login").click(function(){
-    $(".form2").show();
+function register(){
+  $("#loginform").hide();
+  $("#registerform").show();
+}
+
+function updateInfo(){
+
+  var id = $('#id').val();
+  console.log(id);
+
+  $.ajax({
+      url: 'http://localhost:8080/login',
+      type:'POST',
+      data: JSON.stringify({
+          username:  $('#username').val(),
+          password: $('#password').val()
+          }),
+      async: true,
+      contentType:'application/json',
+      success: window.location.reload(),
+      error: errorCallback
   });
+}
