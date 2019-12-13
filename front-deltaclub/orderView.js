2f0bc2bcea84088912eb9af1b16b345fe131e42f
Vasco Sousa <az.thewolf.jr@gmail.com>
2019-12-13 01:05:32 -0100

    further developed orderView

---
+++ front-deltaclub/orderView.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+    $("#loginbutton1").click(function(){
+
+        $.ajax({
+            url: 'http://localhost:8080/login/a',
+            type:'POST',
+            data: JSON.stringify({
+                username:  $('#username').val(),
+                password: $('#password').val()
+                }),
+            async: true,
+            contentType:'application/json',
+            success: successCallback,
+            error: errorCallback
+        });
+
+        function successCallback(){
+            getMainView();
+        };
+
+        function errorCallback(){
+            alert("Invalid credentials!")
+        };
+
+      });
+});
+
+
+function loginshow(){
+    $("#registerform").hide(200);
+    $("#loginform").show(200);
+}
+
+function register(){
+  $("#loginform").hide(200);
+  $("#registerform").show(200);
+}
+
+function submitregist(){
+
+  console.log(JSON.stringify({
+    username:  $('#username').val(),
+    password: $('#password').val()
+    }));
+
+    $.ajax({
+      url: 'http://localhost:8080/login',
+      type:'POST',
+      data: JSON.stringify({
+          username:  $('#username').val(),
+          password: $('#password').val()
+          }),
+      async: true,
+      contentType:'application/json',
+      success: successCallback,
+      error: errorCallback
+  });
+
+  function successCallback(){
+      loginshow();
+  }
+
+  function errorCallback(){
+      alert("error");
+  }
+}
+
+function getMainView(){
+    $("#loginView").hide();
+  $("#mainView").show();
+}
